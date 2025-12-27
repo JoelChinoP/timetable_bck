703a6f5da81ddacad34f59fc45aaba5750eb4ca7
Joel <jchinop@unsa.edu.pe>
2025-12-27 00:26:10 -0500

    feat(graph): initialize GraphQL schema and resolvers for Todo application
    - Added GraphQL schema definition for Todo and User types, including queries and mutations.
    - Implemented basic structure for resolver functions in the schema.resolvers.go file.
    - Generated model types for NewTodo, Todo, and User in models_gen.go.
    - Established a Resolver struct for dependency injection in the application.

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/extension"
+	"github.com/99designs/gqlgen/graphql/handler/lru"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/JoelChinoP/timetable_bck/graph"
+	"github.com/vektah/gqlparser/v2/ast"
+)
+
+const defaultPort = "8080"
 
 func main() {
-	fmt.Println("Timetable backend 🚀")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.New(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	srv.AddTransport(transport.Options{})
+	srv.AddTransport(transport.GET{})
+	srv.AddTransport(transport.POST{})
+
+	srv.SetQueryCache(lru.New[*ast.QueryDocument](1000))
+
+	srv.Use(extension.Introspection{})
+	srv.Use(extension.AutomaticPersistedQuery{
+		Cache: lru.New[string](100),
+	})
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
